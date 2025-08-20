0c769ecd37c7ff0558ec845162203b65973474b7
Ammar <ammaralikhang66pk@gmail.com>
2025-08-21 03:09:37 +0500

    changed packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className=" bg-blue-200 min-h-screen flex flex-col items-center justify-center">
+    <h1 className='text-4xl font-bold text-purple-700'>Hello, world</h1>
+    <p className='text-lg text-gray-600 mt-4'>Welcome to your React app with Tailwind CSS!</p>
+    <button className='mt-6 px-4 py-2 bg-green-500 text-white rounded hover:bg-purple-600'>
+      click Me
+    </button>
+  </div>
+);
+
 
 export default App
